f606843da949899f10bbdb1c0eecdfa05bd443b3
n7eru@vulcan <edwn.njeru@gmail.com>
2023-07-18 17:29:54 +0300

    Add description to transaction entry and opening-balance to transactionAccount

--- src/main/java/io/github/calvary/web/rest/TransactionEntryResource.java
+++ src/main/java/io/github/calvary/web/rest/TransactionEntryResource.java
@@ -16,7 +16,6 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.StreamSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
